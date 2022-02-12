392443381eaa9b0f28a6742ec1dff76becdf7640
Dylan Fruit <dylanfrut@outlook.fr>
2022-02-12 21:11:52 +0100

    test

---
+++ scripts/utils/contactForm.js
@@ -0,0 +1,22 @@
+const modalSubmit = document.querySelector(".contact_button");
+
+function displayModal() {
+    const modal = document.getElementById("contact_modal");
+	modal.style.display = "block";
+}
+
+function closeModal() {
+    const modal = document.getElementById("contact_modal");
+    modal.style.display = "none";
+}
+
+modalSubmit.addEventListener("click", (e) => {
+    e.preventDefault();
+    closeModal();
+});
+
+window.addEventListener("keydown", (e) => {
+    if(e.key === 'Escape'){
+        closeModal();
+    }
+});
